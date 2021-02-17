019c5156a576a1aad2c0f6241261de750ac01f7b
Jacob Smith <jacobs@finelineprintinggroup.com>
2021-02-17 17:34:28 -0500

    starting a simple token parser UI

---
+++ TokenParser/Parser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TokenParser
+{
+    public class Parser
+    {
+        private readonly Encoding encoding;
+        private readonly JwtSecurityTokenHandler handler;
+
+        public Parser(bool useUTF8 = true)
+        {
+            encoding = useUTF8 ? Encoding.UTF8 : Encoding.ASCII;
+            handler = new JwtSecurityTokenHandler();
+        }
+
+        private SymmetricSecurityKey GetKey(string keySecret)
+        {
+            return new SymmetricSecurityKey(encoding.GetBytes(keySecret));
+        }
+
+        public ClaimsPrincipal Parse(string keySecret, string token)
+        {
+            var validations = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = GetKey(keySecret),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+            return handler.ValidateToken(token, validations, out var tokenSecure);
+        } // END Parse
+    }
+}
